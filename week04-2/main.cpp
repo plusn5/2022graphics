34fbb20221299f1d5747c9c16561eb5bdbe51863
plusn <91170882+plusn5@users.noreply.github.com>
2022-03-14 16:43:00 +0800

    Add files via upload

---
+++ week04-2/main.cpp
@@ -0,0 +1,33 @@
+#include <GL/glut.h>
+void myTeapot(float x, float y)
+{
+     glPushMatrix();                ///push備份矩陣
+        glTranslatef( x, y, 0);
+        glutSolidTeapot(0.3);
+     glPopMatrix();                 ///pop還原矩陣
+
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3f(1,0,0);
+
+    myTeapot (+0.5, +0.5);
+    myTeapot (+0.5, -0.5);
+    myTeapot (-0.5, -0.5);
+    myTeapot (-0.5, +0.5);
+
+    glutSwapBuffers();
+}
+int main(int argc, char **argv)
+{
+     glutInit(&argc, argv);
+     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+     glutCreateWindow("week04");
+     glutDisplayFunc(display);
+
+     glutMainLoop();
+
+     return 0;
+
+}
